af47948774a0043c4e0b34aab6f164921fdffe29
Pedro Alves <pedro_alves@me.com>
2018-03-21 02:06:20 -0300

    making seed file to work with data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321035710) do
+ActiveRecord::Schema.define(version: 20180321042004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,9 @@ ActiveRecord::Schema.define(version: 20180321035710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "bike_id"
+    t.bigint "user_id"
     t.index ["bike_id"], name: "index_trips_on_bike_id"
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,4 +76,5 @@ ActiveRecord::Schema.define(version: 20180321035710) do
   add_foreign_key "slots", "bikes"
   add_foreign_key "slots", "stations"
   add_foreign_key "trips", "bikes"
+  add_foreign_key "trips", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  user            =   User.create(
+                        email: Faker::Internet.safe_email,
+                        name: Faker::Simpsons.character,
+                        password: "123!@#",
+                        password_confirmation: "123!@#"
+                      )
+
+  station         = Station.create(
+                      name: Faker::Address.city
+                    )
+
+  bike            = Bike.create(
+                      brand: Faker::DrWho.character,
+                      model: Faker::DrWho.specie,
+                      station_id: station.id
+                    )
+
+  trip            =  Trip.create(
+                      origin_station: station.name,
+                      final_station: Faker::Address.city,
+                      start_date: Faker::Date.between(2.days.ago, Date.today),
+                      end_date: Faker::Date.between(2.days.ago, Date.today),
+                      distance: Faker::Number.decimal(2),
+                      bike_id: bike.id,
+                      user_id: user.id
+                    )
+end
+
+1000.times do
+  Slot.first_or_create(
+      station_id: Station.order("RANDOM()").first.id,
+      available: Faker::Boolean.boolean(0.2),
+      bike_id: Bike.order("RANDOM()").first.id
+    )
+end
